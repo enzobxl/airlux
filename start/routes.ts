07b2b4abea8e012a4c30ca66f39e8abb6712a435
quentin-beaufort <118752155+quentin-beaufort@users.noreply.github.com>
2024-11-07 11:44:04 +0100

    removed docker_compose.yml and merge tags

--- start/routes.ts
+++ start/routes.ts
@@ -10,20 +10,6 @@
 import router from '@adonisjs/core/services/router'
 import AutoSwagger from 'adonis-autoswagger'
 import swagger from '#config/swagger'
-<<<<<<< HEAD
-import RegistersController from '#controllers/registers_controller'
-
-// returns swagger in YAML
-router.get('/swagger', async () => {
-  return AutoSwagger.default.docs(router.toJSON(), swagger)
-})
-
-// Renders Swagger-UI and passes YAML-output of /swagger
-router.get('/docs', async () => {
-  return AutoSwagger.default.ui('/swagger', swagger)
-})
-=======
->>>>>>> 2d17164c57cc2718ecceea46d3ae41f755de1387
 
 const PingController = () => import('#controllers/ping_controller')
 const RegistersController = () => import('#controllers/registers_controller')
@@ -33,10 +19,7 @@ router.get('/swagger', async () => {
   return AutoSwagger.default.docs(router.toJSON(), swagger)
 })
 
-<<<<<<< HEAD
-// Register routes
-router.get('/register', [RegistersController, 'get'])
-=======
+
 // Renders Swagger-UI and passes YAML-output of /swagger
 router.get('/docs', async () => {
   return AutoSwagger.default.ui('/swagger', swagger)
@@ -46,4 +29,3 @@ router.post('/ping', [PingController, 'postPing'])
 
 // Register routes
 router.post('/register', [RegistersController, 'postRegister'])
->>>>>>> 2d17164c57cc2718ecceea46d3ae41f755de1387
